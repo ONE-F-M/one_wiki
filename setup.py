b2c86c1c8fb8955951c71ffedeba28bbbf60aa7d
mymi14s <mymi14s@hotmail.com>
2023-11-15 13:32:45 +0000

    fix:frappe import

--- setup.py
+++ setup.py
@@ -15,11 +15,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	# install_requires=install_requires
+	install_requires=install_requires
 )
-
-# from setuptools import setup
-
-# name = "hrms"
-
-# setup()
