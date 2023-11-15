a1abde032b616168f687448ab6c5e0144ca66fa1
mymi14s <mymi14s@hotmail.com>
2023-11-15 13:26:36 +0000

    fix:frappe import

--- one_wiki/__init__.py
+++ one_wiki/__init__.py
@@ -1,6 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-__version__ = '0.0.1'
-import frappe
 from one_wiki.utils import update_context_
 
 from frappe.website.page_renderers.document_page import DocumentPage
@@ -9,7 +9,7 @@ from wiki.www import drafts
 
 from one_wiki.www.drafts import get_context as get_draft_context
 
-
+__version__ = '0.0.1'
 
 
 drafts.get_context = get_draft_context
