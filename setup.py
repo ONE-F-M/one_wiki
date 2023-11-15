35f56e7598e679a57a958e5c142ec28d5b757ab8
mymi14s <mymi14s@hotmail.com>
2023-11-15 13:31:13 +0000

    fix:frappe import

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in one_wiki/__init__.py
-from one_wiki import __version__ as version
+# from one_wiki import __version__ as version
 
 setup(
 	name="one_wiki",
-	version=version,
+	version="1.0",
 	description="Custom Application to override Frappe Wiki",
 	author="One_FM",
 	author_email="support@one-fm.com",
@@ -17,3 +17,9 @@ setup(
 	include_package_data=True,
 	# install_requires=install_requires
 )
+
+# from setuptools import setup
+
+# name = "hrms"
+
+# setup()
